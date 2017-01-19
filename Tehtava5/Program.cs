3e9a9c2d40cffe98669ec260f7068b17436232c3
K1539 <K1539@ICT-421-WS-30>
2017-01-19 13:59:45 +0200

    updates

---
+++ Tehtava5/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * ehtävässä tulee toteuttaa C#-ohjelma radion perustoimintojen testaamiseen.
+
+Kannettavassa radiossa on vain kolme säädintä: päälle/pois-kytkin, äänen voimakkuuden säädin (arvot 0, 1, 2,..., 9) ja kuunneltavan kanavan taajuusvalinta (2000.0 - 26000.0).
+Laadi luokka kannettavan radion toteutukseksi. Käytä tekemääsi luokkaa pääohjelmasta käsin, eli testaile radion toimivuutta erilaisilla voimakkuuden ja taajuuden arvoilla. 
+Jätä asetus- ja tulostuslauseet näkyville pääohjelmaan, jotta radio-olion käyttö voidaan todentaa.
+
+Jere Liikka 19.1.2017
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace JAMK.IT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Tehtava5/Radio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class Radio
+    {
+        double taajuus;
+        public bool OnOff;
+        int volume;
+        private double MaxTaajuus = 26000.0;
+        private double MinTaajuus = 2000.0;
+        private int MaxVol = 9;
+        private int MinVol = 0;
+        public double Taajuus
+        {
+            get
+            {
+                return taajuus;
+            }
+            set
+            {
+                if (value > MaxTaajuus || value < MinTaajuus)
+                {
+                    if (value > MaxTaajuus)
+                    {
+                        taajuus = MaxTaajuus;
+                        
+                    }
+                    else
+                    {
+                        taajuus = MinTaajuus;
+                    }
+                }
+                else { taajuus = value; }
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                if (value > MaxVol || value < MinVol)
+                {
+                    if (value > MaxVol)
+                    {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
