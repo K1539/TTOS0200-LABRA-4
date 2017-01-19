fa9dbcefd1aedefea79ab922e621d4cdf720438b
jerel <jerel@pytis>
2017-01-20 01:12:46 +0200

    t7

---
+++ Tehtava4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Pohdi jokin reaalimaailman asia, jonka kautta voit toteuttaa pienimuotoisen C#-ohjelman/toteutuksen, joka käyttää perintää.
+ *
+ *Jere Liikka 20.1.2017 
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JAMK.IT;
+
+namespace JAMK.IT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
